cd3930af797f3ff763a9359a115ff469ee98a5b2
DamienJacques900 <damien.jacques900@gmail.com>
2017-01-03 17:09:10 +0100

    test

---
+++ figurine/src/main/java/com/spring/henallux/model/CommandLineWithFigurine.java
@@ -0,0 +1,37 @@
+package com.spring.henallux.model;
+
+public class CommandLineWithFigurine 
+{
+	private CommandLine commandLine;
+	
+	private Figurine figurine;
+
+	public CommandLineWithFigurine(CommandLine commandLine, Figurine figurine) 
+	{
+		this.commandLine = commandLine;
+		this.figurine = figurine;
+	}
+
+	public CommandLine getCommandLine() 
+	{
+		return commandLine;
+	}
+
+	public void setCommandLine(CommandLine commandLine) 
+	{
+		this.commandLine = commandLine;
+	}
+
+	public Figurine getFigurine() 
+	{
+		return figurine;
+	}
+
+	public void setFigurine(Figurine figurine) 
+	{
+		this.figurine = figurine;
+	}
+	
+	
+	
+}
