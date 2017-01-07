a575842b3fcbac9a94d245de2e0fb77a2616c588
DamienJacques900 <damien.jacques900@gmail.com>
2017-01-07 11:17:46 +0100

    test

--- figurine/src/main/java/com/spring/henallux/controller/ConfirmationCommandController.java
+++ figurine/src/main/java/com/spring/henallux/controller/ConfirmationCommandController.java
@@ -15,11 +15,17 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.spring.henallux.dataAccess.dao.CommandDAO;
 import com.spring.henallux.dataAccess.dao.CommandLineDAO;
+import com.spring.henallux.dataAccess.dao.LanguageDAO;
+import com.spring.henallux.dataAccess.dao.TranslationFigurineDAO;
 import com.spring.henallux.model.Command;
 import com.spring.henallux.model.CommandLine;
 import com.spring.henallux.model.CommandLineWithFigurine;
 import com.spring.henallux.model.Figurine;
+import com.spring.henallux.model.Language;
+import com.spring.henallux.model.Promotion;
+import com.spring.henallux.model.TranslationFigurine;
 import com.spring.henallux.model.User;
+import com.spring.henallux.service.PromotionService;
 
 @Controller
 @RequestMapping(value="/confirmationCommand")
@@ -31,6 +37,15 @@ public class ConfirmationCommandController
 	
 	@Autowired
 	private CommandLineDAO commandLineDAO;
+	
+	@Autowired
+	private PromotionService promotionService;
+	
+	@Autowired
+	private TranslationFigurineDAO translationFigurineDAO;
+	
+	@Autowired
+	private LanguageDAO languagesDAO;
 	
 	@ModelAttribute(value=ConnectionController.CURRENTUSER)
 	public User currentUser()
@@ -83,8 +98,28 @@ public class ConfirmationCommandController
 
 	@RequestMapping(value="/commandNotValidate", method=RequestMethod.POST)
 	public String getCommandUserRequired(Model model, @ModelAttribute(value="command") Command command,@ModelAttribute(value=ConnectionController.CURRENTUSER) User currentUser,@ModelAttribute(value=DescriptionController.COMMANDLINES) List<CommandLineWithFigurine> commandLinesWithFigurines
-			,@ModelAttribute(value="figurineBasket") CommandLine commandLine, @ModelAttribute(value="deleteBasket") CommandLine commandLineD)
+			,@ModelAttribute(value="figurineBasket") CommandLine commandLine, @ModelAttribute(value="deleteBasket") CommandLine commandLineD, Locale locale)
 	{
+		Language language = languagesDAO.getLanguageByName(locale.toString());
+		
+		ArrayList<Promotion> currentPromotion = new ArrayList<Promotion>();
+		currentPromotion = promotionService.getPromotionValid();
+		
+		model.addAttribute("promotionAll", currentPromotion);
+		
+		ArrayList<TranslationFigurine> translationFigurines = translationFigurineDAO.getAllTranslationFigurines();
+		ArrayList<TranslationFigurine> translationBasket = new ArrayList<TranslationFigurine>();
+		for(int i = 0; i < commandLinesWithFigurines.size(); i++)
+		{
+			for(int j = 0; j < translationFigurines.size();j++)
+			{
+				if(commandLinesWithFigurines.get(i).getFigurine().getIdFigurine() == translationFigurines.get(j).getFigurine() 
+						&& language.getIdLanguage() == translationFigurines.get(j).getLanguage())
+					translationBasket.add(translationFigurines.get(j));
+			}
+		}
+		
+		model.addAttribute("figurineTranslations", translationBasket);
 		
 		double TotalValue = 0;
 		for(CommandLineWithFigurine entity : commandLinesWithFigurines)
